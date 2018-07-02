538b415a44449d7a78e8600e6d3d899b10e5a835
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-07-02 13:04:25 -0500

    3.7

--- search_zenodo.py
+++ search_zenodo.py
@@ -1,13 +1,20 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 """ examples of searching Zenodo"""
 import pyzenodo3
 
-zen = pyzenodo3.Zenodo()
 
-Recs = zen.search('scivision')
+def main():
+    zen = pyzenodo3.Zenodo()
+
+    Recs = zen.search('scivision')
+    assert Recs is not None
 # %%
-Rec = zen.find_record_by_github_repo('scivision/lowtran')
+    Rec = zen.find_record_by_github_repo('scivision/lowtran')
 
-vers = Rec.get_versions_from_webpage()
+    vers = Rec.get_versions_from_webpage()
 
-print(vers)
+    print(vers)
+
+
+if __name__ == '__main__':
+    main()
